c2bf35dfe524e111f5ad3d3c797279ba52c5df98
Jusouza182 <juliana_s_souza@estudante.sesisenai.org.br>
2024-06-14 04:49:18 -0300

    feat: selecionarPrato

---
+++ Introdução JS/projeto-3-DrivenEats/script.js
@@ -0,0 +1,9 @@
+function selecionarPrato(prato){ // "prato" é o nome  que dei para o parametro "this" que veio do HTML. 
+prato.classList.add('adcBorda'); // como prato já está 'nomeado' é só usar sem colocar em uma variavel. 
+let icon = prato.querySelector('ion-icon'); // para pegar uma div dentro de "prato" é necessário nomea-la em uma variavel. 
+icon.classList.remove('esconder'); // e depois usar normalmente 
+let nomeDoPrato = prato.querySelector('.nomePrato').innerHTML; //salva o nome dos pratos
+console.log(nomeDoPrato); 
+
+return nomeDoPrato; //retorna o nome dos pratos para o código
+}
