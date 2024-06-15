324680b214c296e83d771afe0b1e4b103b83930b
Jusouza182 <juliana_s_souza@estudante.sesisenai.org.br>
2024-06-15 20:00:30 -0300

    feat: adiciona pedido atualizado

--- Introdução JS/projeto-3-DrivenEats/script.js
+++ Introdução JS/projeto-3-DrivenEats/script.js
@@ -4,11 +4,15 @@ let nomeDoPrato = "";
 let bebidaSelecionada = null;
 let pratoSelecionado = null;
 let sobremesaSelecionada = null;
+let precoDoPratoSelecionado = "";
+let precoDaBebidaSelecionado = "";
+let precoDaSobremesaSelecionado = "";
+let valorTotal = 0;
 
 function selecionarPrato(prato) {
   // "prato" é o nome  que dei para o parametro "this" que veio do HTML.
   nomeDoPrato = prato.querySelector(".nomePrato").innerHTML; // como prato já está 'nomeado' é só usar sem colocar em uma variavel.
-
+  let precoPrato = prato.querySelector(".precoPrato").innerHTML; // salva o preço do prato.
   /*
     Se constar nomeDoPrato e já tiver um prato selecionado (ou seja, pratoSelecionado vai deixar de ser null)
     vai remover as propriedades de prato selecionado. Caso contrario adiciona as propriedades de selecionado. 
@@ -25,15 +29,17 @@ function selecionarPrato(prato) {
     let icon = prato.querySelector("ion-icon"); // para pegar uma div dentro de "prato" é necessário nomea-la em uma variavel.
     icon.classList.remove("esconder"); // e depois usar normalmente
     pratoSelecionado = prato; //salva o nome do prato selecionado
+    precoDoPratoSelecionado = precoPrato;
   }
   console.log(nomeDoPrato);
   verificarItensSelecionados(); // chama a função para ver se todos os itens estão selecionados
 
-  return nomeDoPrato; //retorna o nome dos pratos para o código
+  return nomeDoPrato, precoPrato; //retorna o nome dos pratos para o código
 }
 
 function selecionarBebida(bebida) {
   nomeDaBebida = bebida.querySelector(".nomePrato").innerHTML;
+  let precoBebida = bebida.querySelector(".precoPrato").innerHTML;
 
   if (nomeDaBebida !== "") {
     if (bebidaSelecionada !== null) {
@@ -46,18 +52,20 @@ function selecionarBebida(bebida) {
     let icon = bebida.querySelector("ion-icon");
     icon.classList.remove("esconder");
     bebidaSelecionada = bebida;
+    precoDaBebidaSelecionado = precoBebida;
   }
   console.log(nomeDaBebida);
   verificarItensSelecionados();
-  return nomeDaBebida;
+  return nomeDaBebida, precoBebida;
 }
 
 function selecionarSobremesa(sobremesa) {
   nomeDaSobremesa = sobremesa.querySelector(".nomePrato").innerHTML;
+  let precoSobremesa = sobremesa.querySelector(".precoPrato").innerHTML;
 
   if (nomeDaSobremesa !== "") {
     if (sobremesaSelecionada !== null) {
-        sobremesaSelecionada.classList.remove("adcBorda");
+      sobremesaSelecionada.classList.remove("adcBorda");
       let iconAnterior = sobremesaSelecionada.querySelector("ion-icon");
       iconAnterior.classList.add("esconder");
     }
@@ -66,10 +74,11 @@ function selecionarSobremesa(sobremesa) {
     let icon = sobremesa.querySelector("ion-icon");
     icon.classList.remove("esconder");
     sobremesaSelecionada = sobremesa;
+    precoDaSobremesaSelecionado = precoSobremesa;
   }
   console.log(nomeDaSobremesa);
   verificarItensSelecionados();
-  return nomeDaSobremesa;
+  return nomeDaSobremesa, precoSobremesa;
 }
 
 //Responsavel por verificar se todos os itens estão selecionados e motifica o botão de prosseguir.
@@ -81,4 +90,51 @@ function verificarItensSelecionados() {
     botaoPedido.classList.add("botaoSelecionado");
     botaoPedido.innerHTML = "Fechar pedido";
   }
+}
+
+// Responsavel por aparecer a tela de finalizar pedido, somente se todos os itens estão selecionados.
+function botaoPedido(botao) {
+  pedido = document.querySelector(".pedido");
+
+  if (nomeDoPrato !== "" && nomeDaBebida !== "" && nomeDaSobremesa !== "") {
+    //faz aparecer a tela de pedido
+    pedido.classList.remove("esconder");
+
+    // motifica o item e preço no pedido
+    let resumoP = pedido.querySelector(".resumoP");
+    resumoP.innerHTML = `${nomeDoPrato}`;
+    let precoP = pedido.querySelector(".precoP");
+    precoP.innerHTML = `R$${precoDoPratoSelecionado}`;
+
+    let resumoBebida = pedido.querySelector(".resumoBebida");
+    resumoBebida.innerHTML = `${nomeDaBebida}`;
+    let precoBebida = pedido.querySelector(".precoBebida");
+    precoBebida.innerHTML = `R$${precoDaBebidaSelecionado}`;
+
+    let resumoSobremesa = pedido.querySelector(".resumoSobremesa");
+    resumoSobremesa.innerHTML = `${nomeDaSobremesa}`;
+    let precoSobremesa = pedido.querySelector(".precoSobremesa");
+    precoSobremesa.innerHTML = `R$ ${precoDaSobremesaSelecionado}`;
+
+    calcularTotal();
+  }
+}
+
+function calcularTotal() {
+  // soma os valores depois de transforma-los em numeros decimais e mudar virgula para ponto, só assim o PC entende.
+
+  valorTotal =
+    parseFloat(precoDoPratoSelecionado.replace(",", ".")) +
+    parseFloat(precoDaBebidaSelecionado.replace(",", ".")) +
+    parseFloat(precoDaSobremesaSelecionado.replace(",", "."));
+
+  // formata para o valor ser mostrando com virgula
+  let valorFormatado = valorTotal.toLocaleString("pt-BR", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+
+  // mostra o valor total no pedido
+  let total = document.querySelector(".precoTotal");
+  total.innerHTML = `R$ ${valorFormatado}`;
 }
