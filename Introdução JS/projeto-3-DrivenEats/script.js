016eef9b4838bfba2a13889dc34d05c4ae5f4283
Jusouza182 <juliana_s_souza@estudante.sesisenai.org.br>
2024-06-15 17:38:52 -0300

    uptade: apenas um item por seção pode ser selecionado

--- Introdução JS/projeto-3-DrivenEats/script.js
+++ Introdução JS/projeto-3-DrivenEats/script.js
@@ -1,13 +1,31 @@
 let nomeDaBebida = "";
 let nomeDaSobremesa = "";
 let nomeDoPrato = "";
+let bebidaSelecionada = null;
+let pratoSelecionado = null;
+let sobremesaSelecionada = null;
 
 function selecionarPrato(prato) {
   // "prato" é o nome  que dei para o parametro "this" que veio do HTML.
-  prato.classList.add("adcBorda"); // como prato já está 'nomeado' é só usar sem colocar em uma variavel.
-  let icon = prato.querySelector("ion-icon"); // para pegar uma div dentro de "prato" é necessário nomea-la em uma variavel.
-  icon.classList.remove("esconder"); // e depois usar normalmente
-  nomeDoPrato = prato.querySelector(".nomePrato").innerHTML; //salva o nome dos pratos
+  nomeDoPrato = prato.querySelector(".nomePrato").innerHTML; // como prato já está 'nomeado' é só usar sem colocar em uma variavel.
+
+  /*
+    Se constar nomeDoPrato e já tiver um prato selecionado (ou seja, pratoSelecionado vai deixar de ser null)
+    vai remover as propriedades de prato selecionado. Caso contrario adiciona as propriedades de selecionado. 
+    */
+  if (nomeDoPrato !== "") {
+    if (pratoSelecionado !== null) {
+      // Remove a marcação da bebida anteriormente selecionada
+      pratoSelecionado.classList.remove("adcBorda");
+      let iconAnterior = pratoSelecionado.querySelector("ion-icon");
+      iconAnterior.classList.add("esconder");
+    }
+    // Marca a bebida atual como selecionada
+    prato.classList.add("adcBorda");
+    let icon = prato.querySelector("ion-icon"); // para pegar uma div dentro de "prato" é necessário nomea-la em uma variavel.
+    icon.classList.remove("esconder"); // e depois usar normalmente
+    pratoSelecionado = prato; //salva o nome do prato selecionado
+  }
   console.log(nomeDoPrato);
   verificarItensSelecionados(); // chama a função para ver se todos os itens estão selecionados
 
@@ -15,30 +33,48 @@ function selecionarPrato(prato) {
 }
 
 function selecionarBebida(bebida) {
-  bebida.classList.add("adcBorda");
-  let icon = bebida.querySelector("ion-icon");
-  icon.classList.remove("esconder");
   nomeDaBebida = bebida.querySelector(".nomePrato").innerHTML;
+
+  if (nomeDaBebida !== "") {
+    if (bebidaSelecionada !== null) {
+      bebidaSelecionada.classList.remove("adcBorda");
+      let iconAnterior = bebidaSelecionada.querySelector("ion-icon");
+      iconAnterior.classList.add("esconder");
+    }
+
+    bebida.classList.add("adcBorda");
+    let icon = bebida.querySelector("ion-icon");
+    icon.classList.remove("esconder");
+    bebidaSelecionada = bebida;
+  }
   console.log(nomeDaBebida);
   verificarItensSelecionados();
-
   return nomeDaBebida;
 }
 
 function selecionarSobremesa(sobremesa) {
-  sobremesa.classList.add("adcBorda");
-  let icon = sobremesa.querySelector("ion-icon");
-  icon.classList.remove("esconder");
   nomeDaSobremesa = sobremesa.querySelector(".nomePrato").innerHTML;
+
+  if (nomeDaSobremesa !== "") {
+    if (sobremesaSelecionada !== null) {
+        sobremesaSelecionada.classList.remove("adcBorda");
+      let iconAnterior = sobremesaSelecionada.querySelector("ion-icon");
+      iconAnterior.classList.add("esconder");
+    }
+
+    sobremesa.classList.add("adcBorda");
+    let icon = sobremesa.querySelector("ion-icon");
+    icon.classList.remove("esconder");
+    sobremesaSelecionada = sobremesa;
+  }
   console.log(nomeDaSobremesa);
   verificarItensSelecionados();
-
   return nomeDaSobremesa;
 }
 
-//Responsavel por verificar se todos os itens estão selecionados e motifica o botão de prosseguir. 
+//Responsavel por verificar se todos os itens estão selecionados e motifica o botão de prosseguir.
 
-function verificarItensSelecionados() { 
+function verificarItensSelecionados() {
   let botaoPedido = document.querySelector(".botaoPedido");
 
   if (nomeDoPrato !== "" && nomeDaBebida !== "" && nomeDaSobremesa !== "") {
@@ -46,5 +82,3 @@ function verificarItensSelecionados() {
     botaoPedido.innerHTML = "Fechar pedido";
   }
 }
-
-
