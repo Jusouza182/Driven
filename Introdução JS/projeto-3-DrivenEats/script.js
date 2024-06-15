fa3226b41d653bd5d8d6a325fc4f1718d3272e51
Jusouza182 <juliana_s_souza@estudante.sesisenai.org.br>
2024-06-15 16:54:37 -0300

    feat: verificar Itens Selecionados

--- Introdução JS/projeto-3-DrivenEats/script.js
+++ Introdução JS/projeto-3-DrivenEats/script.js
@@ -1,10 +1,15 @@
+let nomeDaBebida = "";
+let nomeDaSobremesa = "";
+let nomeDoPrato = "";
+
 function selecionarPrato(prato) {
   // "prato" é o nome  que dei para o parametro "this" que veio do HTML.
   prato.classList.add("adcBorda"); // como prato já está 'nomeado' é só usar sem colocar em uma variavel.
   let icon = prato.querySelector("ion-icon"); // para pegar uma div dentro de "prato" é necessário nomea-la em uma variavel.
   icon.classList.remove("esconder"); // e depois usar normalmente
-  let nomeDoPrato = prato.querySelector(".nomePrato").innerHTML; //salva o nome dos pratos
+  nomeDoPrato = prato.querySelector(".nomePrato").innerHTML; //salva o nome dos pratos
   console.log(nomeDoPrato);
+  verificarItensSelecionados(); // chama a função para ver se todos os itens estão selecionados
 
   return nomeDoPrato; //retorna o nome dos pratos para o código
 }
@@ -13,8 +18,9 @@ function selecionarBebida(bebida) {
   bebida.classList.add("adcBorda");
   let icon = bebida.querySelector("ion-icon");
   icon.classList.remove("esconder");
-  let nomeDaBebida = bebida.querySelector(".nomePrato").innerHTML;
+  nomeDaBebida = bebida.querySelector(".nomePrato").innerHTML;
   console.log(nomeDaBebida);
+  verificarItensSelecionados();
 
   return nomeDaBebida;
 }
@@ -23,10 +29,22 @@ function selecionarSobremesa(sobremesa) {
   sobremesa.classList.add("adcBorda");
   let icon = sobremesa.querySelector("ion-icon");
   icon.classList.remove("esconder");
-  let nomeDaSobremesa = sobremesa.querySelector(".nomePrato").innerHTML;
+  nomeDaSobremesa = sobremesa.querySelector(".nomePrato").innerHTML;
   console.log(nomeDaSobremesa);
+  verificarItensSelecionados();
 
   return nomeDaSobremesa;
+}
+
+//Responsavel por verificar se todos os itens estão selecionados e motifica o botão de prosseguir. 
+
+function verificarItensSelecionados() { 
+  let botaoPedido = document.querySelector(".botaoPedido");
+
+  if (nomeDoPrato !== "" && nomeDaBebida !== "" && nomeDaSobremesa !== "") {
+    botaoPedido.classList.add("botaoSelecionado");
+    botaoPedido.innerHTML = "Fechar pedido";
+  }
 }
 
 
